ea7447a2a1972733935c87d4384657196a3bf724
cassiemak <cassie.mak@gmail.com>
2014-11-28 14:10:09 +0800

    It works

--- js/application.js
+++ js/application.js
@@ -12,26 +12,42 @@ $(document).ready(function(){
     //   }
 
     $("#calc-prices-button").click(function(){
-      // create a function (when you click, it calculate total price)
+      // create & define a function 
       var calcTotalPrice = function(){
         // do something
         var totalPrice = 0;
-        
         // make a for loop
-        for (var i=0; i < $("items-list.children.item-price").length; i++){
+        for (var i=0; i < $("#items-list .item-price").length; i++){
           // do sth
-          // totalPrice += $("#items-list .item-price")[i]* $("#items-list .item-qty")[i]; 
-          totalPrice += 1; 
+
+          // this is wrong - totalPrice += $("#items-list .item-price")[i].text * $("#items-list .item-qty")[i].text; 
+          // totalPrice += 1; 
+
+          // get price with respective i
+          var price = $("#items-list .item-price")[i];
+          price = $(price).text();
+          console.log(price);
+          price = price.replace ("$"," ");
+          price = Number(price);
+          console.log(price);
+          
+          // get quantity with respective i
+          var numQuantity = $("#items-list .item-qty .quantity ")[i];
+          numQuantity = $(numQuantity).val();
+          console.log(numQuantity);
+
+          //get totalPrice 
+          console.log(totalPrice);
+          totalPrice += (price * numQuantity);
           console.log(totalPrice);
         };
-        // return totalPrice; 
         $("#calc-prices-button").css('background-color', 'red');
         console.log(totalPrice);
+        // replacing the total price column with the total price; 
+        $("#total-price").text("$ " + totalPrice);
       };
-      // call the function
+      // call the function, when you click, it calculate total price
       calcTotalPrice();
-      // console.log(totalPrice);
-
     });
 //closing .ready function 
 });
